fbfa5c6402104f4101dc60fab5ffbeb03b225a78
Ryan Karpeles <ryan@teamodea.com>
2016-04-06 13:47:20 -0500

    Add Google AdWords call tracking script to header

--- wp-content/themes/Avada-Child-Theme/header.php
+++ wp-content/themes/Avada-Child-Theme/header.php
@@ -1140,6 +1140,11 @@
 	<?php echo $smof_data['google_analytics']; ?>
 
 	<?php echo $smof_data['space_head']; ?>
+
+	<!-- Google AdWords Call Tracking Script -->
+	<script type="text/javascript">
+	(function(a,e,c,f,g,b,d){var h={ak:"1071848057",cl:"ghtKCIPwlWUQ-bSM_wM"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[f]||(a[f]=h.ak);b=e.createElement(g);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(g)[0];d.parentNode.insertBefore(b,d);a._googWcmGet=function(b,d,e){a[c](2,b,h,d,null,new Date,e)}})(window,document,"_googWcmImpl","_googWcmAk","script");
+	</script>
 	
 </head>
 <?php
