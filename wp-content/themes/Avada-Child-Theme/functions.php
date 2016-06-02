aa622a2e19fda7e25195d741726784ccc803f1ce
Ryan Karpeles <ryan@teamodea.com>
2016-06-02 00:56:42 -0500

    Add exit intent script, and remove old unbounce script

--- wp-content/themes/Avada-Child-Theme/functions.php
+++ wp-content/themes/Avada-Child-Theme/functions.php
@@ -19,6 +19,7 @@ function avada_child_scripts() {
   wp_enqueue_script( 'main-child', get_stylesheet_directory_uri() . '/js/main-child.js', '', '1.0', true );
   wp_enqueue_script( 'rslides', get_stylesheet_directory_uri() . '/js/responsiveslides.min.js', '', '1.0', true );    
   wp_enqueue_script( 'isotope', get_stylesheet_directory_uri() . '/js/isotope.min.js', '', '1.0', true );
+  wp_enqueue_script( 'bioep', get_stylesheet_directory_uri() . '/js/bioep.min.js', '', '1.0', true );
 }
 add_action('wp_enqueue_scripts', 'avada_child_scripts');
 
