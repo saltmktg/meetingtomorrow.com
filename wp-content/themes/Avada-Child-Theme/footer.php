f3d9275beac94ac6c2d87bc1206b0dc668a94f76
Ryan Karpeles <ryan@teamodea.com>
2016-06-14 06:39:24 -0500

    Update styles on exit intent form

--- wp-content/themes/Avada-Child-Theme/footer.php
+++ wp-content/themes/Avada-Child-Theme/footer.php
@@ -498,17 +498,20 @@
    <script type="text/javascript">
       bioEp.init({
         html: '<div id="bio_ep_content">' +
-        '<img src="http://beeker.io/images/posts/2/tag.png" alt="Claim your discount!" />' +
-        '<span>HOLD ON!</span>' +
-        '<span>Click the button below to get a special discount</span>' +
-        '<span>This offer will NOT show again!</span>' +
-        '<a href="#YOURURLHERE" class="bio_btn">CLAIM YOUR DISCOUNT</a>' +
+        '<img src="https://meetingtomorrow.com/wp-content/themes/Avada-Child-Theme/img/tag-orange.png" alt="Claim your discount!" />' +
+        '<div class="headline">Get 10% off your next order*</div>' +
+        '<input type="email" placeholder="Email Address">' +
+        '<button>Get My Discount Code Now!</button>' +
+        '<div class="subline">Limited time offer: Expires MM/DD/YY<div>' +
+        '<div class="note">*New customers only, min. order of $500.<div>' +
         '</div>',
-        css: '#bio_ep {width: 400px; height: 300px; color: #333; background-color: #fafafa; text-align: center;}' +
-        '#bio_ep_content {padding: 24px 0 0 0; font-family: "Titillium Web";}' +
-        '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
-        '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
-        '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
+        css: '#bio_ep {width: 310px; height: 320px; color: #333; background-color: #fafafa; text-align: center; padding: 25px 22px; border-radius: 4px;}' +
+        '#bio_ep_content {font-family: "Open Sans", Helvetica, Arial, sans-serif;}' +
+        '#bio_ep_content img {width: 82px;}' +
+        '#bio_ep_close {margin: -37px 0 0 -17px; padding-left: 1px; font-size: 13px; font-weight: 400; width: 25px; height: 25px;}' +
+        '#bio_ep_content .headline {display: block; color: #ec7f4c; font-size: 28px; font-weight: 600; line-height: 30px; margin: 10px 0 10px;}' +
+        '#bio_ep_content .subline {margin-top: 5px;}' +
+        '#bio_ep_content .subline, #bio_ep_content .note {display: block; font-size: 13px; line-height: 1.4;}' +
         '.bio_btn {display: inline-block; margin: 18px 0 0 0; padding: 7px; color: #fff; font-size: 14px; font-weight: 600; background-color: #70bb39; border: 1px solid #47ad0b; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 0; text-decoration: none;}',
         fonts: ['//fonts.googleapis.com/css?family=Titillium+Web:300,400,600'],
         cookieExp: 0
