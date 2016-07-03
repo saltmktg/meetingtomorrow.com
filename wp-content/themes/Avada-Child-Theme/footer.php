4ddb7c31ffa0f74f49e2cba5d66e48cbd0b53b2d
Ryan Karpeles <ryan@teamodea.com>
2016-07-03 13:49:53 -0500

    Update exit intent URL

--- wp-content/themes/Avada-Child-Theme/footer.php
+++ wp-content/themes/Avada-Child-Theme/footer.php
@@ -230,7 +230,7 @@
 	        Get a Quick Quote
 	      </div>
 	      <div class="ob-modal-body">
-	        <iframe id="nsIframeExitIntent" src="<?php echo 'https://forms.na2.netsuite.com/app/site/crm/externalleadpage.nl?compid=3373305&formid=66&h=AACffht_JbRmDTPrYhOS7xCYeyKnSLM3M4w%3D&url=' . rawurlencode( get_permalink()); ?>" width="100%" height="350" frameborder="0" scrolling="no"></iframe>
+	        <iframe id="nsIframeExitIntent" src="" width="100%" height="350" frameborder="0" scrolling="no"></iframe>
 
 	        <!-- Form with hidden fields for GA reporting -->
 					<form id="ga-tracking-form" method="POST" name='contactform' onSubmit=""> 
